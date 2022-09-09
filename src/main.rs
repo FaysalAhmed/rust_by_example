2af0f795286884057c47502f2d2fbafc384e7fdf
Faysal Ahmed <faysal.ahmed833@gmail.com>
2022-09-10 01:04:10 +0600

    add guess the number code. add docker compose files.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
-// this is the main function
+// guess the number https://doc.rust-lang.org/book/ch02-00-guessing-game-tutorial.html
+// you have to run it locally as it require some input.
 
-fn main(){
-    println!("Hello World"); // println! is a rust macro.
+// for input scope.
+use std::io;
+
+// this is the main function
+fn main() {
+    // introducing our program with some text. 
+    println!("Guess the number!");
+    println!("Please input your guess");
+
+    // creating a empty instance of String and making the variable mutable so that we can modify it later
+    let mut guess = String::new();
+
+    // reading line from console, if fail, we will show a message "Failed to read line"
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    // showing our input string in console.
+    println!("You guessed: {guess}");
 }
